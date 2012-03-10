9f89d41542d331e1c804ec1b2f024299560c8619
NotMyself <bobby.johnson@gmail.com>
2012-03-10 08:11:22 -0800

    setting up testing

---
+++ src/Test/Reality_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Test
+{
+    [TestFixture]
+    public class Reality_Tests
+    {
+        [Test]
+        public void true_should_be_true()
+        {
+            true.ShouldBe(true);
+        }
+    }
+}
